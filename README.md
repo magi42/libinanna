29574aa81854c88788e658dfba6a736e51d33eb8
Marko Grönroos <magi@iki.fi>
2025-11-10 10:11:40 +0200

    Added a README

---
+++ README.md
@@ -0,0 +1,22 @@
+# libInanna
+
+Marko Grönroos 1998-2005.
+
+libInanna is an Artificial Neural Network computation and learning library for research purposes.
+It uses an object-oriented neural model that allows complex structures.
+
+The library requires [MagiCLib++](/magi42/magiclib).
+It is expected to be compiled under the MagiCLib++ source tree, to be able to use and develop the base library more easily.
+To have it compile there, you need to include it in the `Makefile` of MagiCLib++, with the `makemodules` parameter:
+
+```
+makemodules = libmagic libinanna
+```
+
+Then, you can run `make` to compile both MagiCLib++ and `libinanna`.
+
+## Projects
+
+Under the `extras`, there are applications using the library.
+
+* `prediction` for making time series predictions
---
+++ extras/extras.mk
@@ -0,0 +1,51 @@
+################################################################################
+#    This file is part of the MagiC++ library.                                 #
+#                                                                              #
+#    Copyright (C) 2004 Marko Grönroos <magi@iki.fi>                           #
+#                                                                              #
+################################################################################
+#                                                                              #
+#   This library is free software; you can redistribute it and/or              #
+#   modify it under the terms of the GNU Library General Public                #
+#   License as published by the Free Software Foundation; either               #
+#   version 2 of the License, or (at your option) any later version.           #
+#                                                                              #
+#   This library is distributed in the hope that it will be useful,            #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU          #
+#   Library General Public License for more details.                           #
+#                                                                              #
+#   You should have received a copy of the GNU Library General Public          #
+#   License along with this library; see the file COPYING.LIB.  If             #
+#   not, write to the Free Software Foundation, Inc., 59 Temple Place          #
+#   - Suite 330, Boston, MA 02111-1307, USA.                                   #
+#                                                                              #
+################################################################################
+
+################################################################################
+# Define root directory of the source tree
+################################################################################
+export SRCDIR ?= ../..
+
+################################################################################
+# Define module name and compilation type
+################################################################################
+modname = extras
+modpath = libinanna/extras
+
+################################################################################
+# Include build framework
+################################################################################
+include $(SRCDIR)/build/magicdef.mk
+
+################################################################################
+# Recursively compile some subprojects
+################################################################################
+makemodules = prediction
+
+# Disabled: equalizer
+
+################################################################################
+# Compile
+################################################################################
+include $(SRCDIR)/build/magictop.mk
